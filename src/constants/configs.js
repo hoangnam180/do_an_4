0d5b54ee2e93ec023320068b018020217af2630e
namnh180 <namnh@tekup.vn>
2022-12-01 18:22:52 +0700

    api banner

--- src/constants/configs.js
+++ src/constants/configs.js
@@ -1,5 +1,5 @@
 export const API_SERVER =
-  'https://8938-2402-800-629c-bdc4-f959-846d-5605-5445.ap.ngrok.io';
+  'https://8e34-2405-4802-6056-3ec0-396f-8f1f-d1a8-4b43.ap.ngrok.io';
 export const ACCESS_TOKEN = '_ac_tk_topaz';
 export const USER_INFO = 'user_info';
 export const IS_AUTH = 'is_auth';
